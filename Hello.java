ff81d8c6048e566674ac604206cd7b4b06141d00
2353159253 <152378209+2353159253@users.noreply.github.com>
2023-11-29 16:51:01 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,12 @@ public class Hello
 {
    public static void main(String[] args){
     System.out.println("hello git");
-return;
+      System.out.println("智慧城管系统");
+      System.out.println("智慧执法");
+      System.out.println("圆林绿化");
+      System.out.println("市政环卫");
+       System.out.println("环卫公司——热修分支进行修复bug");
+      System.out.println("dev分支开发功能");
+
 }
 }
